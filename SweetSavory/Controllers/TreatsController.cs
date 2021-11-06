b0622016b0f83ee895056eacdd6f4400cfa69546
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-11-06 14:52:35 -0700

    add index path

---
+++ SweetSavory/Controllers/TreatsController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SweetSavory.Models;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace SweetSavory.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly SweetSavoryContext _db;
+    private readonly UserManager <ApplicationUser> _userManager;
+    public TreatsController(UserManager <ApplicationUser> userManager, SweetSavoryContext db)
+    {
+      _userManager = userManager; 
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Treats.ToList());
+    }
+  }
+}
