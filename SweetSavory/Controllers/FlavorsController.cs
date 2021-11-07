9ee4ad89423d65f4b94421a67268d57e2dcd4108
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-11-06 17:13:27 -0700

    add details route

--- SweetSavory/Controllers/FlavorsController.cs
+++ SweetSavory/Controllers/FlavorsController.cs
@@ -39,6 +39,13 @@ namespace SweetSavory.Controllers
       return RedirectToAction("Index");
     }
 
-    
+    public ActionResult Details(int id)
+    {
+      var thisFlavor= _db.Flavors
+        .Include(flavor => flavor.JoinEntities)
+				.ThenInclude(join => join.Treat)
+				.FirstOrDefault(flavor => flavor.FlavorId ==id);
+			return View(thisFlavor);
+    }
   }
 }
